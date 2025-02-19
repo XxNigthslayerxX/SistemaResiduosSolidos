9b3d8ea406e18dc4a9c2be8c34b5346860ec687e
XxNigthslayerxX <rusbeluceta24@gmail.com>
2025-02-19 14:22:57 -0400

    Actualizaciones
    Registros y interfaz optimizada

--- SistemaGestionResiduos.Infrastructure/obj/Debug/net8.0/SistemaGestionResiduos.Infrastructure.AssemblyInfo.cs
+++ SistemaGestionResiduos.Infrastructure/obj/Debug/net8.0/SistemaGestionResiduos.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestionResiduos.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e47014edacf44428c60abe9d64247fc0eeadf9b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f716a0fb31d26131b951c9d7dbcff9c2c0d7a32")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestionResiduos.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestionResiduos.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
